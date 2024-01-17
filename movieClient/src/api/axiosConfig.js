8ced0393324afa67ae089e7c779b9be4bc4819f5
AmilaWijesinghe <amilanwijesinghe01@gmail.com>
2024-01-17 12:00:12 +0530

    update

--- movieClient/src/api/axiosConfig.js
+++ movieClient/src/api/axiosConfig.js
@@ -1,6 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
+const URL = "http://localhost:8080";
 
-export default axios.create({
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
-    headers: {'ngrok-skip-browser-warning':'true'}
-})
+const api = axios.create({
+  baseURL: URL
+});
+
+export default api;
+
